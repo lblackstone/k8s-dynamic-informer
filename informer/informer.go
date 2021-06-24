0e884323691fccc53d564e3d73f7ebe43b24ff1d
Levi Blackstone <levi@pulumi.com>
2021-06-24 15:45:56 -0600

    Restructure code

--- informer/informer.go
+++ informer/informer.go
@@ -1,62 +1,23 @@
 package informer
 
 import (
-	"flag"
 	"fmt"
-	"path/filepath"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 )
 
-func getDynamicInformer(resourceType string) (informers.GenericInformer, error) {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	cfg, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err)
-	}
-
-	// Grab a dynamic interface that we can create informers from
-	dc, err := dynamic.NewForConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
-	// Create a factory object that can generate informers for resource types
-	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(dc, 0, corev1.NamespaceAll, nil)
-	// "GroupVersionResource" to say what to watch e.g. "deployments.v1.apps" or "pods.v1"
-	gvr, gr := schema.ParseResourceArg(resourceType)
-	if gvr == nil {
-		gvr = &schema.GroupVersionResource{Version: gr.Group, Resource: gr.Resource}
-	}
-	// Finally, create our informer!
-	informer := factory.ForResource(*gvr)
-	return informer, nil
-}
-
-func PodWatcher(namespace string) {
-	//dynamic informer needs to be told which type to watch
-	podInformer, _ := getDynamicInformer("pods.v1")
+func Watch(inf informers.GenericInformer, runner func(<-chan struct{}, cache.SharedIndexInformer)) {
 	stopper := make(chan struct{})
 	defer close(stopper)
-	runPodInformer(stopper, podInformer.Informer(), namespace)
+
+	runner(stopper, inf.Informer())
 }
 
-func runPodInformer(stopCh <-chan struct{}, s cache.SharedIndexInformer, namespace string) {
+func PodLogger(stopCh <-chan struct{}, s cache.SharedIndexInformer) {
 	toPod := func(obj interface{}) *corev1.Pod {
 		d := &corev1.Pod{}
 		err := runtime.DefaultUnstructuredConverter.
@@ -76,7 +37,6 @@ func runPodInformer(stopCh <-chan struct{}, s cache.SharedIndexInformer, namespa
 				ns = "default"
 			}
 			fmt.Printf("Added Pod %s/%s\n", ns, pod.Name)
-			// do what we want with the Pod event
 		},
 		DeleteFunc: func(obj interface{}) {
 			pod := toPod(obj)
--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"flag"
+	"path/filepath"
+
 	"github.com/lblackstone/k8s-dynamic-informer/informer"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	informer.PodWatcher("")
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	cfg, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// Grab a dynamic interface that we can create informers from
+	dc, err := dynamic.NewForConfig(cfg)
+	if err != nil {
+		panic(err)
+	}
+	factory := dynamicinformer.NewDynamicSharedInformerFactory(dc, 0)
+	podInformer := factory.ForResource(schema.GroupVersionResource{Version: "v1", Resource: "pods"})
+	informer.Watch(podInformer, informer.PodLogger)
 	select {}
 }
